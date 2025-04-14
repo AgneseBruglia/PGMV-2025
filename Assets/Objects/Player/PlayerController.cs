608a298e1a66efabb0f53a58c4f37ab135b742ca
David Mendes <djcmendes@gmail.com>
2025-04-14 14:29:25 +0100

    Add flying controls to robot

--- Assets/Objects/Player/PlayerController.cs
+++ Assets/Objects/Player/PlayerController.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 2f;
-    public float jump_force = 5f;
+    public float speed = 5f;
+    public float max_speed = 10f;
+
+    public float jump_force = 2f;
+    public float fall_force = 2f;
+    
     public float turn_rate = 10f;
     [SerializeField] bool use_physics = true;
     private Rigidbody rb;
-    private bool isGrounded;
-    
+
+    public float gravity_force = -10f;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        rb.linearDamping = 0; // config for falling , drag
     }
 
-    // Update is called once per frame
     void Update()
     {
         rb.isKinematic = !use_physics;
@@ -26,41 +30,50 @@ public class PlayerController : MonoBehaviour
         if (!use_physics)
         {
             transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * speed * Time.deltaTime, Space.Self);
-            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * turn_rate * Time.deltaTime, Space.Self);
+            transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * turn_rate  * Time.deltaTime, Space.Self);
         }
         else
         {
+            Debug.Log("Applying forces to go forward and backweards");
+            //rb.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * speed, ForceMode.Force);
+            //rb.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * speed, ForceMode.Force);
             rb.AddRelativeForce(Vector3.forward * Input.GetAxis("Vertical") * speed, ForceMode.Force);
             rb.AddRelativeForce(Vector3.right * Input.GetAxis("Horizontal") * speed, ForceMode.Force);
         }
 
-        //Jump
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        //Limit velocity
+        //Debug.Log(rb.linearVelocity.magnitude);
+        if (rb.linearVelocity.magnitude > max_speed)
         {
-            rb.AddForce(Vector3.up * jump_force, ForceMode.Impulse);
+            rb.linearVelocity = Vector3.ClampMagnitude(rb.linearVelocity, max_speed);
         }
 
-        //Fix position
-        if (transform.position.y < -5)
+        //Jump
+        if (Input.GetKeyDown(KeyCode.Space) && transform.position.y < 50)
+        {
+            //Physics.gravity = new Vector3(0, -9.81f, 0);
+            // We need the vector to have move force than the gravity, ForceMode is applied against mass
+            rb.AddForce(Vector3.up * jump_force * 9.81f * rb.mass, ForceMode.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.Space)) // In Air Acelaration
+        {
+            //Physics.gravity = new Vector3(0, -9.81f, 0);
+            // We need the vector to have move force than the gravity, ForceMode is applied against mass
+            rb.AddForce(Vector3.up * speed * 9.81f * rb.mass, ForceMode.Acceleration);
+        }       
+        else if (Input.GetKeyUp(KeyCode.Space) && transform.position.y < 50) // In Air decelaration
+        {
+            //Physics.gravity = new Vector3(0, -9.81f, 0);
+            // We need the vector to have move force than the gravity, ForceMode is applied against mass
+            rb.AddForce(Vector3.down * fall_force, ForceMode.Impulse);
+        }
+
+        
+        if (transform.position.y < -5) //Fix position if falls belloiw ground
         {
             transform.position = Vector3.zero;
         }
-    }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = true;
-        }
-    }
-
-    void OnCollisionExit(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-        {
-            isGrounded = false;
-        }
     }
 
 }
