55b4eb109bba7430e26de2f1f007a046ef6ef928
David Mendes <djcmendes@gmail.com>
2025-04-28 09:12:26 +0100

    fix cabinet scaling

--- Assets/Objects/Cabinet/Cabinet.cs
+++ Assets/Objects/Cabinet/Cabinet.cs
@@ -9,14 +9,12 @@ public class Cabinet : MonoBehaviour
     public GameObject prefab_p; // prefab_p
     public GameObject prefab_c; // prefabC
     public GameObject prefab_g; // prefabG
-	public GameObject prefab_t_glass; // prefabGlass
     public GameObject shell_corner; // Cabinet part 1,3,7,9
     public GameObject shell_middle; // Cabinet part 2, 4, 6, 8
     public GameObject shell_center; // Cabinet part 5
     public GameObject prefab_door_part; // door child
     private GameObject door; // door parent
     private Light cabinetLight; // Reference to the light component
-
 
     private Vector3 minPosition;
     private Vector3 maxPosition;
@@ -40,7 +38,8 @@ public class Cabinet : MonoBehaviour
         XmlNodeList columns = xml_doc.SelectNodes("/cabinet/column");
 
         door = new GameObject("Door");
-        door.transform.SetParent(transform);
+        door.transform.parent = transform;
+        door.transform.localPosition = new Vector3(0, 0, 0);
 
         Vector3 parent_position = transform.position;
         Quaternion parent_rotation = transform.rotation;
@@ -69,9 +68,6 @@ public class Cabinet : MonoBehaviour
                     case "G":
                         prefab = prefab_g;
                         break;
-					case "T":
-                        prefab = prefab_t_glass;
-                        break;
                     default:
                         prefab = prefab_p; // empty
                         break;
@@ -79,61 +75,114 @@ public class Cabinet : MonoBehaviour
 
                 //get position of the obj that the script is attached to
                 //Buiild interior of the cabinet
-                Vector3 position = transform.position + new Vector3(0, m, n);
+                //Vector3 position = transform.position + new Vector3(0, m, n);
                 
-                Instantiate(prefab, position, Quaternion.identity, transform);
+                //Instantiate(prefab, position, Quaternion.identity, transform);
+
+                Vector3 position = transform.position + new Vector3(0, m, n);
+                GameObject instance = Instantiate(prefab);
+                instance.transform.parent = transform; // Set parent first
+                instance.transform.localRotation = Quaternion.identity; // Adjust local position
+                instance.transform.localPosition = position; // Adjust local position
+                instance.transform.localScale    = new Vector3(1, 1, 1);
+               
+                //door
+                GameObject door_instance = Instantiate(prefab_door_part);
+                door_instance.transform.parent = transform; // Set parent first
+                door_instance.transform.localRotation = Quaternion.Euler(0f, 0f, 0f); // Adjust local position
+                door_instance.transform.localPosition = position + new Vector3(0,0,0); // Adjust local position
+                door_instance.transform.localScale = new Vector3(1, 1, 1);
+                door_instance.transform.parent = door.transform; // Set parent first
 
                 //door
-                GameObject door_part = Instantiate(prefab_door_part, position, Quaternion.Euler(0f, 0f, 0f), door.transform);
-                if (door_part != null)
-                {
-                    //Debug.Log("Instantiated door part successfully.");
-                    door_part.transform.SetParent(door.transform); // Set the parent after instantiation
-                }
-                else
-                {
-                    Debug.LogError("Failed to instantiate door part!");
-                }
+                //GameObject door_part = Instantiate(prefab_door_part, position, Quaternion.Euler(0f, 0f, 0f), door.transform);
 
+                
                 //build corners
                 if (n == 0 && m == m_max) //Build corner 1
                 {
-                    Instantiate(shell_corner, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    //Instantiate(shell_corner, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    GameObject shell_corner_instance = Instantiate(shell_corner);
+                    shell_corner_instance.transform.parent = transform; // Set parent first
+                    shell_corner_instance.transform.localRotation = Quaternion.Euler(0f, 0f, 0f); // Adjust local position
+                    shell_corner_instance.transform.localPosition = position; // Adjust local position
+                    shell_corner_instance.transform.localScale    = new Vector3(1, 1, 1);
                 }
+
                 else if (n == n_max && m == m_max)//Build corner 2
                 {
-                    Instantiate(shell_corner, position, Quaternion.Euler(90f, 0f, 0f), transform);
+                    //Instantiate(shell_corner, position, Quaternion.Euler(90f, 0f, 0f), transform);
+                    GameObject shell_corner2_instance = Instantiate(shell_corner);
+                    shell_corner2_instance.transform.parent = transform; // Set parent first
+                    shell_corner2_instance.transform.localRotation = Quaternion.Euler(90f, 0f, 0f); // Adjust local position
+                    shell_corner2_instance.transform.localPosition = position; // Adjust local position
+                    shell_corner2_instance.transform.localScale    = new Vector3(1, 1, 1);
                 }
                 else if (n == 0 && m == 0)//Build corner 3 (7)
                 {
-                    Instantiate(shell_corner, position, Quaternion.Euler(-90f, 0f, 0f), transform);
+                    //Instantiate(shell_corner, position, Quaternion.Euler(-90f, 0f, 0f), transform);
+                    //removed the bottom
+                    GameObject shell_middle2_instance = Instantiate(shell_middle);
+                    shell_middle2_instance.transform.parent = transform; // Set parent first
+                    shell_middle2_instance.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f); // Adjust local position
+                    shell_middle2_instance.transform.localPosition = position; // Adjust local position
+                    shell_middle2_instance.transform.localScale = new Vector3(1, 1, 1);
                 }
                 else if (n == n_max && m == 0)//Build corner 4 (build 9 if 3x3)
                 {
-                    Instantiate(shell_corner, position, Quaternion.Euler(180f, 0f, 0f), transform); 
+                    //Instantiate(shell_corner, position, Quaternion.Euler(180f, 0f, 0f), transform); 
+                    //removed the bottom
+                    GameObject shell_middle2_instance = Instantiate(shell_middle);
+                    shell_middle2_instance.transform.parent = transform; // Set parent first
+                    shell_middle2_instance.transform.localRotation = Quaternion.Euler(90f, 0f, 0f); // Adjust local position
+                    shell_middle2_instance.transform.localPosition = position; // Adjust local position
+                    shell_middle2_instance.transform.localScale = new Vector3(1, 1, 1);
+                    
                 }
                 // build borders
                 else if (n != 0 && n != n_max && m == m_max) //Build between corner 1 to corner 2 (build 2 if 3x3)
                 {
-                    Instantiate(shell_middle, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    //Instantiate(shell_middle, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    GameObject shell_middle1_instance = Instantiate(shell_middle);
+                    shell_middle1_instance.transform.parent = transform; // Set parent first
+                    shell_middle1_instance.transform.localRotation = Quaternion.Euler(0, 0, 0); // Adjust local position
+                    shell_middle1_instance.transform.localPosition = position; // Adjust local position
+                    shell_middle1_instance.transform.localScale = new Vector3(1, 1, 1);
                 }
                 else if (n == 0 && m != m_max && m != 0)
                 {
                     //Build between corner 1 to corner 3 (build 4 if 3x3)
-                    Instantiate(shell_middle, position, Quaternion.Euler(-90f, 0f, 0f), transform);
+                    //Instantiate(shell_middle, position, Quaternion.Euler(-90f, 0f, 0f), transform);
+                    GameObject shell_middle2_instance = Instantiate(shell_middle);
+                    shell_middle2_instance.transform.parent = transform; // Set parent first
+                    shell_middle2_instance.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f); // Adjust local position
+                    shell_middle2_instance.transform.localPosition = position; // Adjust local position
+                    shell_middle2_instance.transform.localScale = new Vector3(1, 1, 1);
                 }
                 else if (n == n_max && m != m_max && m != 0)
                 {
                     //Build between corner 2 to corner 4 (build 6 if 3x3)
-                    Instantiate(shell_middle, position, Quaternion.Euler(90f, 0f, 0f), transform);
+                    //Instantiate(shell_middle, position, Quaternion.Euler(90f, 0f, 0f), transform);
+                    GameObject shell_middle3_instance = Instantiate(shell_middle);
+                    shell_middle3_instance.transform.parent = transform; // Set parent first
+                    shell_middle3_instance.transform.localRotation = Quaternion.Euler(90f, 0f, 0f); // Adjust local position
+                    shell_middle3_instance.transform.localPosition = position; // Adjust local position
+                    shell_middle3_instance.transform.localScale = new Vector3(1, 1, 1);
                 }
-                else if (n != 0 && n != n_max && m == 0) //Build between corner 3 to corner 4 (build 8 if 3x3)
+                /* removed the bootom
+                 * else if (n != 0 && n != n_max && m == 0) //Build between corner 3 to corner 4 (build 8 if 3x3)
                 {
-                    Instantiate(shell_middle, position, Quaternion.Euler(180f, 0f, 0f), transform);
+                    //Instantiate(shell_middle, position, Quaternion.Euler(180f, 0f, 0f), transform);
                 }
+                */
                 else
                 {
-                    Instantiate(shell_center, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    //Instantiate(shell_center, position, Quaternion.Euler(0f, 0f, 0f), transform);
+                    GameObject shell_center_instance = Instantiate(shell_center);
+                    shell_center_instance.transform.parent = transform; // Set parent first
+                    shell_center_instance.transform.localRotation = Quaternion.Euler(90f, 0f, 0f); // Adjust local position
+                    shell_center_instance.transform.localPosition = position; // Adjust local position
+                    shell_center_instance.transform.localScale = new Vector3(1, 1, 1);
                 }
 
                 m++;
